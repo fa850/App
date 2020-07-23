693fe6b36f59875dde3557d7ebee30ace06b378e
fa082281 <fa082281@W1744947.northamerica.cerner.net>
2020-07-23 13:38:04 -0500

    Commiting chnage3 to BranchX

--- App/src/FirstApp/FirstAppHello.java
+++ App/src/FirstApp/FirstAppHello.java
@@ -5,7 +5,8 @@ public class FirstAppHello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Change 1");
-
+		System.out.println("Change 2");
+		System.out.println("Change 3");
 	}
 
 }
